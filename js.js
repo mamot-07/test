907d0d0d12525d06f71461b3dafd182fca29b4c2
mamot-07 <gokudara5923@gmail.com>
2025-02-07 11:47:11 +0900

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,23 @@
-$(function(){
-    $(".pager>ul>li").click(function(){
-         $(this).addClass("on").siblings().removeClass("on");
-    })
+ $(function(){
+
+  $(".pager>ul>li").click(function(){
+    $(this).addClass("on").siblings().removeClass("on");
+})
+    $('.autoplay').slick(
+      {dots: true,
+        autoplay: true,
+        utoplaySpeed: 1000,
+        
+      }
+    );
     });
+    // $('.autoplay').slick({
+    //   slidesToShow: 3,
+    //   slidesToScroll: 1,
+    //   autoplay: true,
+    //   autoplaySpeed: 2000,
+    // });
+          
+
+
+ 
